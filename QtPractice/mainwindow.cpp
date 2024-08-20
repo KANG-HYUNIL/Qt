28423bf1f28880e7779e24774e4980facb2d0c4e
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2024-08-20 17:54:39 +0900

    MainWindow code analysis
    :: 이용한 생성자, 소멸자 및 상속 개념을 인터넷에서 확인해 주석으로 대충 적음

--- QtPractice/mainwindow.cpp
+++ QtPractice/mainwindow.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+//생성자 호출,
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent) //QMainWindow 생성자 호출(상속받기에 부모 클래스 호출)
+
+    //MainWindow의 멤버 변수 ui를 초기화, Ui::MainWindow 생성 후 ui에 할당
+    //이를 통해 ui 변수는 Ui::MainWindow를 가리키게 됨
     , ui(new Ui::MainWindow)
+
 {
     ui->setupUi(this);
 }
